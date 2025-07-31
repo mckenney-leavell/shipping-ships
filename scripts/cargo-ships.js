fe2d09c31e38f95c64658f44d75e5eafd2ce17b9
McKenney Leavell <maleavell@gmail.com>
2025-07-31 15:59:32 -0500

    finished

--- scripts/cargo-ships.js
+++ scripts/cargo-ships.js
@@ -1,47 +1,43 @@
-import { getShips, getHaulers } from "./database.js"
-
+import { getShips, getHaulers } from "./database.js";
 
 export const shipList = () => {
-    const ships = getShips()
+  const ships = getShips();
 
-    let shipHTML = "<ul>"
+  let shipHTML = "<ul>";
 
-    for (const ship of ships) {
-        // Convert each ship object to an <li> and append to the shipHTML string
-        shipHTML += `
+  for (const ship of ships) {
+    // Convert each ship object to an <li> and append to the shipHTML string
+    shipHTML += `
             <li class="line-item" data-type="shipping" data-shipName="${ship.name}" data-haulerId="${ship.haulerId}">${ship.name}</li>    
-        `
+        `;
+  }
+
+  shipHTML += "</ul>";
+
+  return shipHTML;
+};
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+  const shippingShipName = itemClicked.dataset.shipname;
+  // Was a shipping ship list item clicked?
+  if (itemClicked.dataset.type === "shipping") {
+    // Get the haulerId value of the shipping ship clicked
+    const haulerId = parseInt(itemClicked.dataset.haulerid);
+    // Define a default object for the found hauler
+    let haulingShip = { name: "Incorrect" };
+
+    // Iterate the array of hauler objects
+    const allHaulers = getHaulers();
+    for (const hauler of allHaulers) {
+      // Does the haulerId foreign key match the id of the current hauler?
+      if (hauler.id === haulerId) {
+        // Reassign the value of `haulingShip` to the current hauler
+        haulingShip.name = hauler.name;
+      }
     }
-
-    shipHTML += "</ul>"
-
-    return shipHTML
-}
-
-document.addEventListener(
-    "click",
-    (clickEvent) => {
-        const itemClicked = clickEvent.target
-        const shippingShipName = itemClicked.dataset.shipname
-        // Was a shipping ship list item clicked?
-        if (itemClicked.dataset.type === "shipping") {
-            // Get the haulerId value of the shipping ship clicked
-            const haulerId = parseInt(itemClicked.dataset.haulerid)
-            // Define a default object for the found hauler
-            let haulingShip = { name: "Incorrect" }
-
-            // Iterate the array of hauler objects
-            const allHaulers = getHaulers()  
-                for (const hauler of allHaulers) {
-                    // Does the haulerId foreign key match the id of the current hauler? 
-                    if (hauler.id === haulerId) {
-                    // Reassign the value of `haulingShip` to the current hauler
-                        haulingShip.name = hauler.name
-                    }
-                }
-            // Show an alert to the user with this format...
-            // Palais Royal is being hauled by Seawise Giant
-            window.alert(`${shippingShipName} is being hauled by ${haulingShip.name}`)
-        }
-    }
-)
+    // Show an alert to the user with this format...
+    // Palais Royal is being hauled by Seawise Giant
+    window.alert(`${shippingShipName} is being hauled by ${haulingShip.name}`);
+  }
+});
